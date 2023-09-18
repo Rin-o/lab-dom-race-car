8d2182d0480d5e3268a7ec0694121f4cafe2307f
Rin-o <rino091370@gmail.com>
2023-09-18 22:16:55 +0200

    draft

--- js/game.js
+++ js/game.js
@@ -1,3 +1,31 @@
 class Game {
-    // code to be added
+    constructor() {
+      this.startScreen = document.getElementById("game-intro");
+      this.gameScreen = document.getElementById("game-screen");
+      this.gameEndScreen = document.getElementById("game-end");
+      this.height = 600;
+      this.width = 500;
+    }
+    start() {
+        // Set the height and width of the game screen
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+
+             // Hide the start screen
+      this.startScreen.style.display = "none";
+      
+      // Show the game screen
+      this.gameScreen.style.display = "block";
+
+    // Start the game loop
+    this.gameLoop();
+    }  
+
+    gameLoop() {
+        this.update();
 }
+
+update() {
+  console.log("in the update");
+}
+}  
--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-window.onload = function () {
+window.addEventListener('load',()=>{
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
-  startButton.addEventListener("click", function () {
+  function startGame() {
+    console.log('start game');
+    //document.getElementById ('game-intro').style.diplay = 'none'
+    //document.getElementById ('game-screen').style.diplay = 'block'
+    const game = new Game ()
+    game.start()
+  }
+
+  startButton.addEventListener('click', function () {
     startGame();
   });
-
-  function startGame() {
-    console.log("start game");
-  }
-};
+})
